06b76a16783a6ce29ea31d7bd783fa9ee6eebe24
ggichuru <ggichuru.dev@gmail.com>
2022-08-16 03:48:30 +0300

    [tests] -> initiliazer base test

--- programs/gif-portal/src/lib.rs
+++ programs/gif-portal/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7eDtFtH92kuc8dwNgAvoL2dKfbVauELpVcSpbWdF4vX4");
 
 #[program]
 pub mod gif_portal {
--- tests/gif-portal.ts
+++ tests/gif-portal.ts
@@ -1,16 +1,36 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { GifPortal } from "../target/types/gif_portal";
 
-describe("gif-portal", () => {
-  // Configure the client to use the local cluster.
+const main = async () => {
+  console.log("😀 Starting Test");
+
+  /**
+   * @description tell anchor to set our provider
+   *  - It gets the data from `solana config get`
+   *  - In this case, its grabbing our local env, for anchor to know it should run the code locally
+   */
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.GifPortal as Program<GifPortal>;
+  /**
+   * @description tell anchor to automatically compile our code in lib.rs and get it deployed on a local validator.
+   */
+  const program = anchor.workspace.GifPortal;
+  const tx = await program.rpc.initialize();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("🤖 Your transaction Signature", tx);
+};
+
+/**
+ * @description run the main function asynchronously
+ */
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
